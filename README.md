d5cf1f8698ebcd68ddad68145dad30c54720831d
JP-Butler <justinpbutler12@gmail.com>
2023-06-27 21:16:17 -0400

    add completed README.md, checked other files for errors/cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# sqlalchemy-challenge
+# sqlalchemy-challenge Module 10
+------------------------------------------------------
+
+## Honolulu, Hawaii Vacation Analysis
+This challenge used the sql-alchemy concepts learned in Module 10 - Advanced SQL of the Carelton Data Analytics Boot Camp such as creating classes, ORMs, Flask routes and APIs to name a few. Putting these new skills to the test I performed a climate analysis on my desired vacation destination, Honolulu, Hawaii!
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started & Installing](#getting-started--installing)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+## About 
+As the description above stated, to complete my analysis and make sure I familiarized myself with the climate I performed a thorough analysis of the temperature observation (tobs), precipitation (prcp) and weather station data housed in the sqlite database ***hawaii.sqlite***. In the first portion of the challenge I used functions such as create_engine() and automap_base() to connect to the database and automatically reflect the tables in the database into classes for easy querying later on. A session was then created to query the data, queries such as filtering for data such of the the prcp values for the previous 12 months. Familiar ***Python*** libraries such as pandas and matplotlib were then used to display statistics and visuals like a summary statistics table (using the .describe() function) and a histogram (pd plot method). <br>  
+In the second portion, I designed a Climate App in ***Flask API*** format with various routes that can return requested data when accessing the given route in the URL bar. Several static routes were created to return information such as a JSON list of the different stations in the database along with their information. Two dynamic routes were designed to take input from the api user. For e.g. when a start date (in "%m%d%Y" format) was given the min, max and avg of tobs data greater than & equal to that date was provided. The second route accepts a start & end date and provides the same min, max & avg for the tobs date between and including those dates.
+
+## Getting Started & Installing
+See [Usage](#usage) for installing guide/links.
+The following are needed to run script successfully:  
+1. virtual development environment running Python version 3 
+2. Python Libraries:
+    -matplotlib
+    -numpy
+    -sqlalchemy
+    -datetime
+    -flask
+3.**NOTE** verify you are in the folder that contains the app.py file before running. The terminal will likey render an error otherwise, and flask link (http://127.0.0.1:5000) will be inacessible.
+
+## Usage
+1. Download and install Anaconda for Python 3.[Download Anaconda](https://www.anaconda.com/distribution/#windows)
+2. Most Python libraries should be installed once the Anaconda download is complete. To verify library packages, run the following in the ***Anaconda Prompt***:
+'''
+conda list
+'''
+
+## Contributing
+Justin Butler
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,12 +1,11 @@
-# Import the dependencies.
+# Import the dependencies
 import numpy as np
-
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 #################################################
@@ -25,15 +24,11 @@ measurement_table = Base.classes.measurement
 # reference variable for station table
 station_table = Base.classes.station
 
-
-
-
 #################################################
 # Flask Setup
 #################################################
 
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
@@ -41,23 +36,29 @@ app = Flask(__name__)
  
 @app.route('/')
 def welcome():
-    #list availible apis
+    #list availible api - routes
     return(
-        f"Welcome to the Climate App!<br>"
-        f"Available Routes below:<br>"
+        f"<h1>Welcome to the Climate App!<h1/>"
+        f"<h2>Available Routes below:<h2/>"
+        f"1)&nbsp&nbspPrecipitation data for last 12 months<br>"
         f"/api/v1.0/precipitation<br>"
+        f"2)&nbsp&nbspStation list of Hawaii Database<br>"
         f"/api/v1.0/stations<br>"
+        f"3)&nbsp&nbspTemperature Observations (tobs) of the most-active station for the previous year of data<br>"
         f"/api/v1.0/tobs<br>"
+        f"4)&nbsp&nbspReturn the min, max and average temperature for all dates >= a specific start date<br>"
         f"/api/v1.0/&lt;start&gt;<br>"
+        f"5)&nbsp&nbspReturn the min, max and average temperature for all dates between a specific start and end date, inclusive<br>"
         f"/api/v1.0/&lt;start&gt/&lt;end&gt;<br>"
     )
 
-# define precipitation route
+# define precipitation route for precipiation analysis of last 12 months of data
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
     
+    #query DB for precipitation data 
     year_prior_dt = dt.date(2017,8,23) - dt.timedelta(days=365)
     data_results = session.query(measurement_table.date, measurement_table.prcp).filter\
     (measurement_table.date >= year_prior_dt).all()
@@ -70,12 +71,13 @@ def precipitation():
     return jsonify(precip_data)
 
 
-# define stations route
+# Define stations route for stations in the DB
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #query DB for station data
     station_list = session.query(station_table.station, station_table.name, \
                                  station_table.latitude, station_table.longitude, \
                                  station_table.elevation).all()
@@ -84,10 +86,75 @@ def stations():
     
     session.close()
     return jsonify({"list of available stations" : all_stations})
-    #return jsonify(list_of_available_stations = all_stations)
+    
+
+# Define Temperature Observations (tobs) route of most active station for previous year
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    # Query lines for tobs of most active station
+    active_station_list = session.query(measurement_table.station, 
+                          func.count(measurement_table.station))\
+                          .group_by(measurement_table.station)\
+                          .order_by(func.count(measurement_table.station).desc()).all()
+    year_prior_date = dt.date(2017,8,18) - dt.timedelta(days=365)
+    tobs_data_results = session.query(measurement_table.date, measurement_table.tobs).filter\
+                                 (measurement_table.station == 'USC00519281')\
+                                 .filter(measurement_table.date >= year_prior_date).all()
+
+    # Iterate over rows to create list of dictionaries, organized by date
+    tobs_list = [{'date': row.date, 'tobs': row.tobs} for row in tobs_data_results]
+
+    #close session & return a JSONified list
+    session.close()
+    return jsonify(tobs_list)
+
+
+
+# Dynamic routes for start date & start/end date parameters
+@app.route('/api/v1.0/<start>')
+@app.route("/api/v1.0/<start>/<end>")
+def tobs_by_date_range(start= None, end= None):
+
 # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+
+    # values returned if only start date given
+    if end == None:
+        #format start_date from string to date type
+        start_date = dt.datetime.strptime(start, "%m%d%Y").date()
+        #query for temp data(min,max & avg) after and including start_date
+        tobs_data = session.query(func.min(measurement_table.tobs), \
+                                func.max(measurement_table.tobs), \
+                                func.avg(measurement_table.tobs))\
+                                .filter(measurement_table.date >= start_date).all()
+            
+        
+        # format data ouput into a list, close session then return a json list
+        list_tobs= list(np.ravel(tobs_data))
+        session.close()
+        return jsonify(list_tobs)
+        # Values returned when start & end date provided
+    else: 
+        #format start & end_date from string to date type
+        start_date = dt.datetime.strptime(start, "%m%d%Y").date()
+        #query for temp data (min,max & avg) in between and including exact date range
+        end_date = dt.datetime.strptime(end, "%m%d%Y").date()
+        tobs_data = session.query(func.min(measurement_table.tobs), \
+                                func.max(measurement_table.tobs), \
+                                func.avg(measurement_table.tobs))\
+                                .filter(measurement_table.date >= start_date)\
+                                .filter(measurement_table.date <= end_date).all()
+            
+        # format data ouput into a list, close session then return a json list  
+        list_tobs= list(np.ravel(tobs_data))
+        session.close()
+        return jsonify(list_tobs)
 
 
 
+# Run the Flask app in debug mode when script is executed as the main module
 if __name__ == '__main__':
     app.run(debug=True)
